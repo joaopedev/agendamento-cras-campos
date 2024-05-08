c440c36b26afca0d52b243f87844f9ba0f0f8978
Matheus Carvalho <matheus_psilveira@hotmail.com>
2024-05-08 17:00:46 -0300

    CSS home page.

---
+++ src/components/SidebarS.tsx
@@ -0,0 +1,68 @@
+import { Button, Text, Stack, Box, Image } from '@chakra-ui/react';
+
+export const SidebarS: React.FC = () => {
+	const sideBtnStyle = {
+		textColor: 'white',
+		fontSize: '28',
+		h: 'fit-content',
+		p: '10px 15px',
+		borderRadius: '0',
+		bgColor: '#ffffff50',
+		w: '100%',
+		justifyContent: 'left',
+		_hover: {
+			color: '#1C75BC',
+			bg: '#ebedf090',
+		},
+		_focus: {
+			boxShadow: 'dark-lg',
+			color: '#1C75BC',
+			bg: 'white',
+		},
+	};
+
+	return (
+		<Box
+			w={'20%'}
+			h={'100vh'}
+			borderRadius="0 100px 0 0 "
+			bgImage={
+				'https://www.turismo.rj.gov.br/wp-content/uploads/2020/03/Rio-Para%C3%ADba-do-Sul-1.jpg'
+			}
+			bgSize={'cover'}
+			bgPos={'20%'}
+		>
+			<Stack
+				p={'150px 0 40px '}
+				justifyContent="space-between"
+				bg="hsla(207, 74%, 42%, 0.85)"
+				w="20%"
+				h="100%"
+				borderRadius="0 100px 0 0 "
+				alignItems="center"
+				boxSize={'full'}
+				// backdropFilter="auto"
+				// backdropContrast="10%"
+			>
+				<Stack p=" 0" w={'100%'}>
+					<Button sx={sideBtnStyle}>Início</Button>
+
+					<Button sx={sideBtnStyle}>Agendamento</Button>
+				</Stack>
+				<Stack alignItems={'center'} mt={'100px'}>
+					<Image
+						boxSize={'100px'}
+						objectFit="contain"
+						src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Bras%C3%A3o_de_Campos_dos_Goytacazes.png"
+						alt="Brasão de Campos dos Goytacazes"
+					/>
+					<Text fontSize={'12'} color={'white'} textAlign={'center'} maxW={'150px'}>
+						Prefeitura Municipal de Campos dos Goytacazes
+					</Text>
+				</Stack>
+			</Stack>
+		</Box>
+	);
+};
+
+export default SidebarS;
--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,49 +1,30 @@
-import React from "react";
-import {
-  Button,
-  FormControl,
-  FormLabel,
-  Input,
-  VStack,
-  Box,
-} from "@chakra-ui/react";
+import React from 'react';
+import { Button, FormControl, FormLabel, Input, VStack, Box } from '@chakra-ui/react';
+import { FaBold } from 'react-icons/fa';
 export const LoginForm: React.FC = () => {
+	return (
+		<Box bg="white" m={2} p={3} rounded="md">
+			<VStack>
+				<FormControl justifyContent="space-between">
+					<VStack></VStack>
+					<VStack>
+						<FormLabel color="black" m="0" fontSize={36} fontWeight="bold">
+							ENTRAR
+						</FormLabel>
+					</VStack>
+					<VStack>
+						<Input mt={5} type="email" placeholder="CPF" borderColor="black" />
+					</VStack>
 
-
-  return (
-    <Box bg="white" m={2} p={3} rounded="md">
-      <VStack>
-        <FormControl justifyContent="space-between">
-          <VStack>
-          </VStack>
-          <VStack>
-            <FormLabel color="black" mt={8} mb={5}>
-              Enter the email used for the purchase!
-            </FormLabel>
-          </VStack>
-          <VStack>
-            <Input
-              mt={5}
-              type="email"
-              placeholder="Type your e-mail"
-               borderColor="black"      
-            />
-          </VStack>
-          
-          <VStack>
-            <Button
-              type="submit"
-              marginTop={6}
-              backgroundColor="grey"
-
-            >
-              Login
-            </Button>
-          </VStack>
-        </FormControl>
-      </VStack>
-    </Box>
-  );
+					<VStack>
+						<Button type="submit" marginTop={6} backgroundColor="#2CA1FF" textColor="white">
+							Login
+						</Button>
+					</VStack>
+				</FormControl>
+			</VStack>
+		</Box>
+	);
 };
 
 export default LoginForm;

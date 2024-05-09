fe63e8d1897dc69c7c339a604b4499c09499e4d2
Matheus Carvalho <matheus_psilveira@hotmail.com>
2024-05-09 17:51:55 -0300

    agendamento.

--- src/pages/SchedulingPage.tsx
+++ src/pages/SchedulingPage.tsx
@@ -2,15 +2,27 @@ import { Button, Flex, Stack, Box } from '@chakra-ui/react';
 import React from 'react';
 import { SidebarS } from '../components/SidebarS';
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
-import LoginForm from '../components/loginForm';
-// import
+// import { Calendar, momentLocalizer } from 'react-big-calendar';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 
 const Scheduling: React.FC = () => {
+	const localizer = momentLocalizer(moment);
 	return (
-		<Flex h="100vh">
-			<SidebarS />
-			<Box></Box>
-		</Flex>
+		<>
+			<Flex h="100vh">
+				<SidebarS />
+				<Box></Box>
+			</Flex>
+			<Flex>
+				<Calendar
+					localizer={localizer}
+					startAccessor="start"
+					endAccessor="end"
+					style={{ height: 500 }}
+				/>
+			</Flex>
+		</>
 	);
 };
 

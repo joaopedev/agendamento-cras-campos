fdb49824819875fbb9e84f379b668a679e915a24
Matheus Carvalho <matheus_psilveira@hotmail.com>
2024-05-14 17:57:42 -0300

    feat: link botões

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from '../pages/loginPage';
 import Home from '../pages/homePage';
-import Scheduling from '../pages/SchedulingPage';
+import SchedulingPage from '../pages/SchedulingPage';
 
 const AppRoutes: React.FC = () => {
 	return (
@@ -10,7 +10,7 @@ const AppRoutes: React.FC = () => {
 			<Routes>
 				<Route path="/" element={<LoginPage />} />
 				<Route path="/home" element={<Home />} />
-				<Route path="/agendamento" element={<Scheduling />} />
+				<Route path="/agendamento" element={<SchedulingPage />} />
 			</Routes>
 		</BrowserRouter>
 	);

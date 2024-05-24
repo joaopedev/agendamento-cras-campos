052e81a42132bee9bc598531d553072fa6507751
Arthur-Acha <acha.arthur@gmail.com>
2024-05-24 15:52:24 -0300

    Nova login page e atualização de rotas

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from '../pages/loginPage';
+import LoginFuncionario from '../pages/loginFuncionario';
 import Home from '../pages/homePage';
 import HomeADM from '../pages/homeADM';
 import HomeFuncionario from '../pages/homeFuncionario';
 import SchedulingPage from '../pages/SchedulingPage';
+import Agendamentos from '../pages/agendamentosPage';
 import Cadastro from '../pages/cadastroPage';
 
 const AppRoutes: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Login />} />
+        <Route path='/funcionario' element={<LoginFuncionario />} />
         <Route path='/home' element={<Home />} />
         ;
         <Route path='/home-adm' element={<HomeADM />} />
         <Route path='/home-funcionario' element={<HomeFuncionario />} />
         <Route path='/agendamento' element={<SchedulingPage />} />
+        <Route path='/agendamentos' element={<Agendamentos />} />
         <Route path='/cadastro' element={<Cadastro />} />
       </Routes>
     </BrowserRouter>

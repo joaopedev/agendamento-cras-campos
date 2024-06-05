22f3a80d8b72b38889b1ff874adbbce866d359cb
Matheus Carvalho <matheus_psilveira@hotmail.com>
2024-06-05 16:08:00 -0300

    fix: responsividade sidebar e menu hamburguer

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -1,74 +1,10 @@
-// import { Menu, MenuButton, MenuList, MenuItem, Box, IconButton } from '@chakra-ui/react';
-// import { NavLink } from 'react-router-dom';
-// import { HamburgerIcon } from '@chakra-ui/icons';
-
-// export const HamburgerMenu: React.FC = () => {
-// 	return (
-// 		<Box display={['flex', 'none', 'none', 'none']} pos={'fixed'}>
-// 			<Menu>
-// 				<MenuButton
-// 					as={IconButton}
-// 					aria-label="Options"
-// 					icon={<HamburgerIcon color={'white'} />}
-// 					bg={'#2CA1FF'}
-// 					variant="outline"
-// 					m={'10px'}
-// 					boxShadow={'1px 1px 2px hsla(0, 28%, 0%, 0.7)'}
-// 					border={'none'}
-// 					_hover={{
-// 						bg: '#1C75BC',
-// 						fontWeight: 'bold',
-// 					}}
-// 					_active={{}}
-// 				/>
-// 				<MenuList bg={'#2CA1FF'}>
-// 					<NavLink to={'/home'}>
-// 						<MenuItem
-// 							bg={'none'}
-// 							_hover={{
-// 								bg: '#1C75BC',
-// 								fontWeight: 'bold',
-// 							}}
-// 							color={'white'}
-// 						>
-// 							Início
-// 						</MenuItem>
-// 					</NavLink>
-
-// 					<NavLink to={'/agendamento'}>
-// 						<MenuItem
-// 							bg={'none'}
-// 							_hover={{
-// 								bg: '#1C75BC',
-// 								fontWeight: 'bold',
-// 							}}
-// 							color={'white'}
-// 						>
-// 							Agendamento
-// 						</MenuItem>
-// 					</NavLink>
-// 				</MenuList>
-// 			</Menu>
-// 		</Box>
-// 	);
-// };
-import React from 'react';
 import { Menu, MenuButton, MenuList, MenuItem, Box, IconButton } from '@chakra-ui/react';
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { HamburgerIcon } from '@chakra-ui/icons';
 
 export const HamburgerMenu: React.FC = () => {
-	const location = useLocation();
-	const currentPath = location.pathname;
-
-	const isAdminPage = currentPath.startsWith('/adm') || currentPath.startsWith('/home-adm');
-	const isEmployeePage =
-		currentPath.startsWith('/home-funcionario') ||
-		currentPath.startsWith('/adm') ||
-		currentPath.startsWith('/home-adm');
-
 	return (
-		<Box display={['flex', 'none', 'none', 'none']} pos={'absolute'}>
+		<Box display={['flex', 'none', 'none', 'none']} zIndex={200} top={0} pos={'fixed'}>
 			<Menu>
 				<MenuButton
 					as={IconButton}
@@ -99,48 +35,18 @@ export const HamburgerMenu: React.FC = () => {
 						</MenuItem>
 					</NavLink>
 
-					{isEmployeePage ? (
-						<NavLink to={'/agendamentos'}>
-							<MenuItem
-								bg={'none'}
-								_hover={{
-									bg: '#1C75BC',
-									fontWeight: 'bold',
-								}}
-								color={'white'}
-							>
-								Agendamentos
-							</MenuItem>
-						</NavLink>
-					) : (
-						<NavLink to={'/agendamento'}>
-							<MenuItem
-								bg={'none'}
-								_hover={{
-									bg: '#1C75BC',
-									fontWeight: 'bold',
-								}}
-								color={'white'}
-							>
-								Agendamento
-							</MenuItem>
-						</NavLink>
-					)}
-
-					{isAdminPage && (
-						<NavLink to={'/adm'}>
-							<MenuItem
-								bg={'none'}
-								_hover={{
-									bg: '#1C75BC',
-									fontWeight: 'bold',
-								}}
-								color={'white'}
-							>
-								Administração
-							</MenuItem>
-						</NavLink>
-					)}
+					<NavLink to={'/agendamento'}>
+						<MenuItem
+							bg={'none'}
+							_hover={{
+								bg: '#1C75BC',
+								fontWeight: 'bold',
+							}}
+							color={'white'}
+						>
+							Agendamento
+						</MenuItem>
+					</NavLink>
 				</MenuList>
 			</Menu>
 		</Box>

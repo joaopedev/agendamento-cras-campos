0ba84b39e8f3a6a70053254dc112c4029cda235a
joaopedev <joaopedev@outlook.com>
2024-06-27 20:27:52 -0300

    Adicionado Dashboard com dados reais.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,82 +1,94 @@
-import React, { useMemo } from 'react';
-import { Flex, Box } from '@chakra-ui/react';
-import CrasPieChart from '../components/CrasPieChart';
-import { crasData } from '../components/CrasData';
-import SelecionarDiaDashboard from '../components/SelecionarDiaDashboard';
-import ScrollUpButton from '../components/ScrollUpButton';
-import { HamburgerMenu } from '../components/HamburgerMenu';
-import SidebarHome from '../components/SidebarHome';
-
-interface Cras {
-  nome: string;
-  data: {
-    name: string;
-    value: number;
-    color: string;
-  }[];
-}
+import React, { useEffect, useState, useContext } from "react";
+import { Flex, Box } from "@chakra-ui/react";
+import CrasPieChart, {
+  PieChartData,
+  CrasData,
+} from "../components/CrasPieChart";
+import { AuthContext } from "../context/AuthContext";
+import { Cras } from "../interface/User";
+import { ISchedulingModel } from "../interface/Schedulling";
+import SidebarHome from "../components/SidebarHome";
+import { HamburgerMenu } from "../components/HamburgerMenu";
+import ScrollUpButton from "../components/ScrollUpButton";
 
 export const Dashboard: React.FC = () => {
-  const totalData = useMemo(() => {
-    const initialTotal = crasData[0].data.map(item => ({
-      ...item,
-      value: 0,
-    })); // Create initialTotal from first cras data
+  const { getAllSchedulling: getSchedulling, payload } = useContext(AuthContext);
+  const [schedullingData, setSchedullingData] = useState<ISchedulingModel[]>([]);
+  const [crasData, setCrasData] = useState<CrasData[]>([]);
+  const [totalData, setTotalData] = useState<PieChartData[]>([
+    { name: "Agendamentos Pendentes", value: 0, color: "#2CA1FF" },
+    { name: "Atendimentos Realizados", value: 0, color: "#38A169" },
+    { name: "Ausentes", value: 0, color: "#FF5757" },
+  ]);
 
-    return crasData.reduce((acc, cras) => {
-      cras.data.forEach((item, index) => {
-        acc[index].value += item.value;
+  useEffect(() => {
+    const fetchSchedullingData = async () => {
+      if (payload) {
+        const response = await getSchedulling();
+        setSchedullingData(response.agendamentos);
+      }
+    };
+
+    fetchSchedullingData();
+  }, [payload, getSchedulling]);
+
+  useEffect(() => {
+    const processCrasData = () => {
+      const dataByCras: { [key: number]: PieChartData[] } = {};
+      const totalDataTemp = [
+        { name: "Agendamentos Pendentes", value: 0, color: "#2CA1FF" },
+        { name: "Atendimentos Realizados", value: 0, color: "#38A169" },
+        { name: "Ausentes", value: 0, color: "#FF5757" },
+      ];
+
+      schedullingData.forEach((agendamento) => {
+        if (!dataByCras[agendamento.cras]) {
+          dataByCras[agendamento.cras] = [
+            { name: "Agendamentos Pendentes", value: 0, color: "#2CA1FF" },
+            { name: "Atendimentos Realizados", value: 0, color: "#38A169" },
+            { name: "Ausentes", value: 0, color: "#FF5757" },
+          ];
+        }
+
+        if (agendamento.status === 2) {
+          dataByCras[agendamento.cras][0].value += 1; // Pendentes
+          totalDataTemp[0].value += 1;
+        }
+        if (agendamento.status === 1) {
+          dataByCras[agendamento.cras][1].value += 1; // Realizados
+          totalDataTemp[1].value += 1;
+        }
+        if (agendamento.status === 3) {
+          dataByCras[agendamento.cras][2].value += 1; // Ausentes
+          totalDataTemp[2].value += 1;
+        }
       });
-      return acc;
-    }, initialTotal);
-  }, []);
 
-  const formattedTotalData: Cras[] = [{ nome: 'Total', data: totalData }];
+      const processedData = Object.entries(dataByCras).map(([key, value]) => ({
+        nome: Cras[Number(key)],
+        data: value,
+      }));
+
+      setCrasData(processedData);
+      setTotalData(totalDataTemp);
+    };
+
+    processCrasData();
+  }, [schedullingData]);
 
   return (
-    <Flex h='100vh' flexDir={'column'}>
+    <Flex h="100vh" flexDir={"column"}>
       <SidebarHome />
       <HamburgerMenu />
       <ScrollUpButton />
-      <SelecionarDiaDashboard />
-      <Box pl={['0%', '30%', '25%', '20%']} w='100%'>
-        <CrasPieChart crasData={formattedTotalData} crasNome='Total' />
-        {crasData.map(cras => (
-          <CrasPieChart
-            key={cras.nome}
-            crasData={[cras]}
-            crasNome={cras.nome}
-          />
+      <Box pl={["0%", "30%", "25%", "20%"]} w="100%">
+        <CrasPieChart crasData={[{ nome: "Total", data: totalData }]} crasNome="Total" />
+        {crasData.map((cras) => (
+          <CrasPieChart key={cras.nome} crasData={[cras]} crasNome={cras.nome} />
         ))}
       </Box>
     </Flex>
   );
 };
 
-export const boxStyle = {
-  w: '60%',
-  maxW: ['300px', '350px', '500px', '950px'],
-  minW: '250px',
-  boxShadow: '2px 2px 5px hsla(0, 28%, 0%, 0.5)',
-  p: ['20px', '20px', '30px', '40px'],
-  borderRadius: '25px',
-  bg: '#F4F4F4',
-  textAlign: 'center',
-  alignContent: 'center',
-};
-export const btnStyle = {
-  p: '0',
-  w: '30%',
-  display: '-ms-grid',
-  boxShadow: '1px 1px 2px hsla(0, 28%, 0%, 0.7)',
-  color: '#fff',
-  bg: '#2CA1FF',
-  maxW: '950px',
-  minW: ['150px', '200px', '250px', '300px'],
-  fontSize: ['0.7rem', '0.8rem', '0.9rem', '1rem'],
-  _hover: {
-    bg: '#1C75BC',
-    fontWeight: 'bold',
-  },
-};
 export default Dashboard;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Login from '../pages/loginPage';
+import LoginFuncionario from '../pages/loginFuncionario';
 import Home from '../pages/homePage';
 import SchedulingPage from '../pages/SchedulingPage';
 import ControleFuncionarios from '../pages/controleFuncionarios';
@@ -23,11 +24,20 @@ const AppRoutes: React.FC = () => {
             isAuthenticated ? <Navigate to='/home' replace /> : <Login />
           }
         />
-
         <Route
           path='/cadastro'
           element={
             isAuthenticated ? <Navigate to='/home' replace /> : <Cadastro />
+          }
+        />{' '}
+        <Route
+          path='/funcionario'
+          element={
+            isAuthenticated ? (
+              <Navigate to='/home' replace />
+            ) : (
+              <LoginFuncionario />
+            )
           }
         />
         {isAuthenticated && (

d070d931773a920c88057fbf23fd61dd280df6c4
joaopedev <joaopedev@outlook.com>
2024-06-28 00:16:39 -0300

    Fix local API for test production.

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3005",
+  baseURL: String(process.env.REACT_APP_API_URL),
   timeout: 5000,
   headers: {'AUTHORIZATION': 'OSS'}
 });

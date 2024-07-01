a2c6d9872339c65148bdb9408930103d6554365a
joaopedev <joaopedev@outlook.com>
2024-07-01 04:07:24 -0300

    Adicionado botão sair no menu lateral.

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -1,103 +1,131 @@
-import { Menu, MenuButton, MenuList, MenuItem, Box, IconButton } from '@chakra-ui/react';
-import { NavLink } from 'react-router-dom';
-import { HamburgerIcon } from '@chakra-ui/icons';
-import { useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Box,
+  IconButton,
+  Button,
+} from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 export const HamburgerMenu: React.FC = () => {
-	const { payload } = useContext(AuthContext);
+  const { payload, signOut } = useContext(AuthContext);
 
-	return (
-		<Box display={['flex', 'none', 'none', 'none']} zIndex={200} top={0} pos={'fixed'}>
-			<Menu>
-				<MenuButton
-					as={IconButton}
-					aria-label="Options"
-					icon={<HamburgerIcon color={'white'} />}
-					bg={'#2CA1FF'}
-					variant="outline"
-					m={'10px'}
-					boxShadow={'1px 1px 2px hsla(0, 28%, 0%, 0.7)'}
-					border={'none'}
-					_hover={{
-						bg: '#1C75BC',
-						fontWeight: 'bold',
-					}}
-					_active={{}}
-				/>
-				<MenuList bg={'#2CA1FF'}>
-					<NavLink to={'/home'}>
-						<MenuItem
-							bg={'none'}
-							_hover={{
-								bg: '#1C75BC',
-								fontWeight: 'bold',
-							}}
-							color={'white'}
-						>
-							Início
-						</MenuItem>
-					</NavLink>
+  const buttonSingleOut = () => {
+    signOut();
+    window.location.href = "/";
+  };
 
-					<NavLink to={'/agendamento'}>
-						<MenuItem
-							bg={'none'}
-							_hover={{
-								bg: '#1C75BC',
-								fontWeight: 'bold',
-							}}
-							color={'white'}
-						>
-							Agendar
-						</MenuItem>
-					</NavLink>
+  return (
+    <Box
+      display={["flex", "none", "none", "none"]}
+      zIndex={200}
+      top={0}
+      pos={"fixed"}
+    >
+      <Menu>
+        <MenuButton
+          as={IconButton}
+          aria-label="Options"
+          icon={<HamburgerIcon color={"white"} />}
+          bg={"#2CA1FF"}
+          variant="outline"
+          m={"10px"}
+          boxShadow={"1px 1px 2px hsla(0, 28%, 0%, 0.7)"}
+          border={"none"}
+          _hover={{
+            bg: "#1C75BC",
+            fontWeight: "bold",
+          }}
+          _active={{}}
+        />
+        <MenuList bg={"#2CA1FF"}>
+          <NavLink to={"/home"}>
+            <MenuItem
+              bg={"none"}
+              _hover={{
+                bg: "#1C75BC",
+                fontWeight: "bold",
+              }}
+              color={"white"}
+            >
+              Início
+            </MenuItem>
+          </NavLink>
 
-					{payload?.tipo_usuario !== 1 && (
-						<NavLink to={'/controleFuncionarios'}>
-							<MenuItem
-								bg={'none'}
-								_hover={{
-									bg: '#1C75BC',
-									fontWeight: 'bold',
-								}}
-								color={'white'}
-							>
-								Agendamentos
-							</MenuItem>
-						</NavLink>
-					)}
+          <NavLink to={"/agendar"}>
+            <MenuItem
+              bg={"none"}
+              _hover={{
+                bg: "#1C75BC",
+                fontWeight: "bold",
+              }}
+              color={"white"}
+            >
+              Agendar
+            </MenuItem>
+          </NavLink>
 
-					{payload?.tipo_usuario === 3 && (
-						<NavLink to={'/dashboard'}>
-							<MenuItem
-								bg={'none'}
-								_hover={{
-									bg: '#1C75BC',
-									fontWeight: 'bold',
-								}}
-								color={'white'}
-							>
-								Dashboard
-							</MenuItem>
-						</NavLink>
-					)}
+          {payload?.tipo_usuario !== 1 && (
+            <NavLink to={"/agendamentos"}>
+              <MenuItem
+                bg={"none"}
+                _hover={{
+                  bg: "#1C75BC",
+                  fontWeight: "bold",
+                }}
+                color={"white"}
+              >
+                Agendamentos
+              </MenuItem>
+            </NavLink>
+          )}
 
-					{payload?.tipo_usuario === 3 && (
-						<NavLink to={'/gerenciamento'}>
-							<MenuItem
-								bg={'none'}
-								_hover={{
-									bg: '#1C75BC',
-									fontWeight: 'bold',
-								}}
-								color={'white'}
-							>
-								Gerenciamento
-							</MenuItem>
-						</NavLink>
-					)}
-				</MenuList>
-			</Menu>
-		</Box>
-	);
+          {payload?.tipo_usuario === 3 && (
+            <NavLink to={"/dashboard"}>
+              <MenuItem
+                bg={"none"}
+                _hover={{
+                  bg: "#1C75BC",
+                  fontWeight: "bold",
+                }}
+                color={"white"}
+              >
+                Dashboard
+              </MenuItem>
+            </NavLink>
+          )}
+
+          {payload?.tipo_usuario === 3 && (
+            <NavLink to={"/gerenciamento"}>
+              <MenuItem
+                bg={"none"}
+                _hover={{
+                  bg: "#1C75BC",
+                  fontWeight: "bold",
+                }}
+                color={"white"}
+              >
+                Gerenciamento
+              </MenuItem>
+            </NavLink>
+          )}
+            <Button
+              bg={"none"}
+              _hover={{
+                bg: "#1C75BC",
+                fontWeight: "bold",
+              }}
+              color={"white"}
+            >
+              Sair
+            </Button>
+        </MenuList>
+      </Menu>
+    </Box>
+  );
 };

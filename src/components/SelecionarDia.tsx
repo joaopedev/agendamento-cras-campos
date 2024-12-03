0908926f6200bba3ba52b7d5cb7dec60c1361d8b
Arthur-Acha <acha.arthur@gmail.com>
2024-12-03 20:31:38 -0300

    horarios finais

--- src/components/SelecionarDia.tsx
+++ src/components/SelecionarDia.tsx
@@ -289,9 +289,9 @@ const SelecionarDia: React.FC = () => {
       // { hora: '13:30', disponivel: true },
       { hora: '14:00', disponivel: true },
       // { hora: '14:30', disponivel: true },
-      { hora: '15:00', disponivel: true },
+      // { hora: '15:00', disponivel: true },
       // { hora: '15:30', disponivel: true },
-      { hora: '16:00', disponivel: true },
+      // { hora: '16:00', disponivel: true },
       // { hora: '16:30', disponivel: true },
     ];
 
